ff51401286f9c585d6f93f7547eba259bbcb7aa0
Smruti234 <89955654+Smruti234@users.noreply.github.com>
2021-09-30 18:33:26 +0530

    Update FlipCoin.java

--- FlipCoin.java
+++ FlipCoin.java
@@ -1,29 +1,27 @@
 package CoreJavaProblem;
 import java.util.Scanner;
-public class FlipCoin {
-			public static void main(String[] args) {
-				int headsCount = 0;
-				int tailsCount = 0;
-				int counter=1;
-				Scanner scanner = new Scanner(System.in);
-				System.out.println("Enter Number of Times you want to Flip coin :");
-				int noOfFlips = scanner.nextInt();
-				scanner.close();
-				while(counter <= noOfFlips)
+
+public class PrimeFactor {
+          public static void main(String[] args) {
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Enter a Number");
+			int number = scanner.nextInt();
+			int isPrime = 0;
+			int countOne,countTwo;
+			for(countOne = 2;(countOne) <= number;countOne++)
+			{
+				if(number % countOne == 0)			
 				{
-					counter++;	
-					double random = Math.random();
-					if(random < 0.5)
-						tailsCount++;
-					else
-						headsCount++;
+				isPrime=1;
+				for(countTwo = 2;countTwo <= (countOne / 2);countTwo++)		
+				{
+					if((countOne % countTwo) == 0)		
+						isPrime = 0;
 				}
-				System.out.println("Number of Times Heads comes :" + headsCount);
-				System.out.println("Number of Times Tails comes :" + tailsCount);
-				double tailsPercentage = (double)tailsCount / noOfFlips * 100;
-				double headsPercentage = (double)headsCount / noOfFlips * 100;
-				System.out.println("Heads Percentage is :" + headsPercentage);
-				System.out.println("Tails Percentage is :" + tailsPercentage);
+				if(isPrime == 1)
+					System.out.println("Prime factor is :" + countOne);
+			
+				}
 			}
 		}
-
+	}
