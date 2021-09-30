03063d409c5d0907f52c4f511a5449f0f1df62ff
Smruti <lenkasmruti123@gmail.com>
2021-09-30 17:56:11 +0530

    Added: LeapYear

---
+++ LeapYear.java
@@ -0,0 +1,24 @@
+package CoreJavaProblem;
+import java.util.Scanner;
+
+public class LeapYear {
+		public static void main(String[] args) {
+			
+			int y;
+			System.out.println("Enter Any Year");
+			Scanner r = new Scanner (System.in);
+			y= r.nextInt();
+			r.close();
+			if(y%100 ==0 && y%400 ==0 || y%100!= 0 && y%4 ==0)
+			{
+				System.out.println("Leap Year");
+			}
+			else
+			{
+				System.out.println("Not Leap Year");
+			}
+			
+		}
+	}
+		
+
