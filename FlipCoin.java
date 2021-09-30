64ee02e08a51343637e5fd7051f9b813e7fb6fda
Smruti234 <89955654+Smruti234@users.noreply.github.com>
2021-09-30 18:44:56 +0530

    Update FlipCoin.java

--- FlipCoin.java
+++ FlipCoin.java
@@ -1,29 +1,17 @@
 package CoreJavaProblem;
-import java.util.Scanner;
-public class FlipCoin {
-			public static void main(String[] args) {
-				int headsCount = 0;
-				int tailsCount = 0;
-				int counter=1;
-				Scanner scanner = new Scanner(System.in);
-				System.out.println("Enter Number of Times you want to Flip coin :");
-				int noOfFlips = scanner.nextInt();
-				scanner.close();
-				while(counter <= noOfFlips)
-				{
-					counter++;	
-					double random = Math.random();
-					if(random < 0.5)
-						tailsCount++;
-					else
-						headsCount++;
-				}
-				System.out.println("Number of Times Heads comes :" + headsCount);
-				System.out.println("Number of Times Tails comes :" + tailsCount);
-				double tailsPercentage = (double)tailsCount / noOfFlips * 100;
-				double headsPercentage = (double)headsCount / noOfFlips * 100;
-				System.out.println("Heads Percentage is :" + headsPercentage);
-				System.out.println("Tails Percentage is :" + tailsPercentage);
-			}
-		}
+
+public class QuotientandRemainder {
+	public static void main(String[] args) {
+
+	    int dividend = 25, divisor = 4;
+
+	    int quotient = dividend / divisor;
+	    int remainder = dividend % divisor;
+
+	    System.out.println("Quotient = " + quotient);
+	    System.out.println("Remainder = " + remainder);
+	  }
+	}
+	
+
 
