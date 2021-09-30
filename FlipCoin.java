64d1d5f21522da162b17d9a89f3a18707f2dbdc3
Smruti234 <89955654+Smruti234@users.noreply.github.com>
2021-09-30 19:12:36 +0530

    Update FlipCoin.java

--- FlipCoin.java
+++ FlipCoin.java
@@ -1,29 +1,15 @@
 package CoreJavaProblem;
-import java.util.Scanner;
-public class FlipCoin {
-			public static void main(String[] args) {
-				int headsCount = 0;
-				int tailsCount = 0;
-				int counter=1;
-				Scanner scanner = new Scanner(System.in);
-				System.out.println("Enter Number of Times you want to Flip coin :");
-				int noOfFlips = scanner.nextInt();
-				scanner.close();
-				while(counter <= noOfFlips)
-				{
-					counter++;	
-					double random = Math.random();
-					if(random < 0.5)
-						tailsCount++;
-					else
-						headsCount++;
-				}
-				System.out.println("Number of Times Heads comes :" + headsCount);
-				System.out.println("Number of Times Tails comes :" + tailsCount);
-				double tailsPercentage = (double)tailsCount / noOfFlips * 100;
-				double headsPercentage = (double)headsCount / noOfFlips * 100;
-				System.out.println("Heads Percentage is :" + headsPercentage);
-				System.out.println("Tails Percentage is :" + tailsPercentage);
-			}
-		}
+
+public class VowelOrConsonant {
+	public static void main(String[] args) {
+
+        char ch = 'w';
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+            System.out.println(ch + " is vowel");
+        else
+            System.out.println(ch + " is consonant");
+
+    }
+}
 
