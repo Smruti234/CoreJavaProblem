8389ddc6c6140f588829828ac899330c3c80aa68
Smruti234 <89955654+Smruti234@users.noreply.github.com>
2021-09-30 18:51:48 +0530

    Update FlipCoin.java

--- FlipCoin.java
+++ FlipCoin.java
@@ -1,29 +1,20 @@
 package CoreJavaProblem;
 import java.util.Scanner;
-public class FlipCoin {
-			public static void main(String[] args) {
-				int headsCount = 0;
-				int tailsCount = 0;
-				int counter=1;
-				Scanner scanner = new Scanner(System.in);
-				System.out.println("Enter Number of Times you want to Flip coin :");
-				int noOfFlips = scanner.nextInt();
-				scanner.close();
-				while(counter <= noOfFlips)
-				{
-					counter++;	
-					double random = Math.random();
-					if(random < 0.5)
-						tailsCount++;
-					else
-						headsCount++;
-				}
-				System.out.println("Number of Times Heads comes :" + headsCount);
-				System.out.println("Number of Times Tails comes :" + tailsCount);
-				double tailsPercentage = (double)tailsCount / noOfFlips * 100;
-				double headsPercentage = (double)headsCount / noOfFlips * 100;
-				System.out.println("Heads Percentage is :" + headsPercentage);
-				System.out.println("Tails Percentage is :" + tailsPercentage);
-			}
-		}
+
+public class SwapTwoNumber {
+	public static void main(String[] args) {  
+	       int x, y, z;// x and y are to swap   
+	       Scanner sc = new Scanner(System.in);  
+	       System.out.println("Enter the value of X and Y");  
+	       x = sc.nextInt();  
+	       y = sc.nextInt();  
+	       System.out.println("before swapping numbers: "+x +"  "+ y);  
+	       /*swapping */  
+	       z = x;  
+	       x = y;  
+	       y = z;  
+	       System.out.println("After swapping: "+x +"   " + y); 
+	    }    
+	}  
+
 
