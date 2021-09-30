163c54d1348f6dc504ef714148807673661e3f95
Smruti234 <89955654+Smruti234@users.noreply.github.com>
2021-09-30 17:59:26 +0530

    Update FlipCoin.java

--- FlipCoin.java
+++ FlipCoin.java
@@ -1,29 +1,22 @@
 package CoreJavaProblem;
 import java.util.Scanner;
-public class FlipCoin {
-			public static void main(String[] args) {
-				int headsCount = 0;
-				int tailsCount = 0;
-				int counter=1;
-				Scanner scanner = new Scanner(System.in);
-				System.out.println("Enter Number of Times you want to Flip coin :");
-				int noOfFlips = scanner.nextInt();
-				scanner.close();
-				while(counter <= noOfFlips)
-				{
-					counter++;	
-					double random = Math.random();
-					if(random < 0.5)
-						tailsCount++;
-					else
-						headsCount++;
-				}
-				System.out.println("Number of Times Heads comes :" + headsCount);
-				System.out.println("Number of Times Tails comes :" + tailsCount);
-				double tailsPercentage = (double)tailsCount / noOfFlips * 100;
-				double headsPercentage = (double)headsCount / noOfFlips * 100;
-				System.out.println("Heads Percentage is :" + headsPercentage);
-				System.out.println("Tails Percentage is :" + tailsPercentage);
-			}
-		}
 
+public class LeapYear {
+		public static void main(String[] args) {
+			
+			int y;
+			System.out.println("Enter Any Year");
+			Scanner r = new Scanner (System.in);
+			y= r.nextInt();
+			r.close();
+			if(y%100 ==0 && y%400 ==0 || y%100!= 0 && y%4 ==0)
+			{
+				System.out.println("Leap Year");
+			}
+			else
+			{
+				System.out.println("Not Leap Year");
+			}
+			
+		}
+	}
